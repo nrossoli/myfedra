19479361bf120b3c1778e636effc74ca115c52a7
valeri <valeri@96872706-0045-0410-9534-84be8447c86c>
2024-07-23 12:08:11 +0000

    svn-github merging clash created problems for tracking is fixed now
    git-svn-id: https://emulsion.na.infn.it/svn/fedra/trunk@1575 96872706-0045-0410-9534-84be8447c86c

--- src/libScan/EdbScanTracking.cxx
+++ src/libScan/EdbScanTracking.cxx
@@ -632,12 +632,12 @@ void EdbScanTracking::TrackSetBT(EdbID idset, TEnv &env, Int_t ix, Int_t iy)
       
         EdbPattern p;
 	p.SetScanID(*id);
-        all_volume_segments += eSproc->ReadPatCPnopar(p,*id, cut, do_erase);
+        //all_volume_segments += eSproc->ReadPatCPnopar(p,*id, cut, do_erase);
         int nread = eSproc->ReadPatCPnopar(p,*id, cut, do_erase);
 	all_volume_segments += nread;
         if (nread == 0){
-	printf("skipping plate, %i, nread is zero", id->ePlate);
-	continue;
+	  printf("skipping plate, %i, nread is zero", id->ePlate);
+	  continue;
 	}
         p.SetZ(plate->Z());
         p.SetSegmentsZ();
